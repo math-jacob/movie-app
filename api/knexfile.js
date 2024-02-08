6466ae5f7f732f49bc3edba8c2b6dcd5000d4b8c
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-08 12:44:04 -0300

    installed sqlite3 and knex, configured knex

---
+++ api/knexfile.js
@@ -0,0 +1,48 @@
+const path = require('path')
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, 'src', 'database', 'database.db')
+    },
+    pool: {
+      afterCreate: (conn, callback) => conn.run("PRAGMA foreign_keys = ON", callback)
+    },
+    useNullAsDefault: true
+  },
+
+  // staging: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // },
+
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+
+};
