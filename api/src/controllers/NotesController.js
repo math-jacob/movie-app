0c0d3eae3bf94ff6c84e3b75970579c6c70f3a3c
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-09 10:48:43 -0300

    add create on NotesController

--- api/src/controllers/NotesController.js
+++ api/src/controllers/NotesController.js
@@ -1,6 +1,33 @@
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
+
 class NotesController {
   async create(request, response) {
-    
+    const { title, description, rating, tags } = request.body
+    const { user_id } = request.params
+
+    if (rating < 1 | rating > 5) {
+      throw new AppError('A nota para o filme deve estar entre 1 e 5.')
+    }
+
+    const [note_id] = await knex('notes').insert({
+      title,
+      description,
+      rating,
+      user_id,
+    })
+
+    const tagsInsert = tags.map(tag => {
+      return {
+        name: tag,
+        note_id,
+        user_id
+      }
+    })
+
+    await knex('tags').insert(tagsInsert)
+
+    return response.status(201).json()
   }
 }
 
