3c9fa08aa8fcc69899327d1bdab62167e5392684
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-09 09:55:29 -0300

    add update on UserController and fixed hash function without await

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -12,7 +12,7 @@ class UserController {
       throw new AppError("Este e-mail já está em uso.")
     }
 
-    const hashedPassword = hash(password, 8)
+    const hashedPassword = await hash(password, 8)
 
     await knex('users').insert({
       name,
@@ -21,6 +21,54 @@ class UserController {
     })
 
     return response.status(201).json()
+  }
+
+  async update(request, response) {
+    const { name, email, password, old_password } = request.body
+    const { id } = request.params
+
+    const [user] = await knex('users').where({ id })
+    console.log(user)
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado.')
+    }
+
+    const [userWithUpdateEmail] = await knex('users').where({ email })
+    
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+      throw new AppError('Este e-mail já está em uso.')
+    }
+
+    user.name = name ?? user.name
+    user.email = email ?? user.email
+
+    if (password && !old_password) {
+      throw new AppError('Você precisa informar a senha antiga.')
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere.')
+      }
+
+      user.password = await hash(password, 8)
+    }
+
+    const new_name = user.name
+    const new_email = user.email
+    const new_password = user.password
+    await knex('users')
+      .where({ id })
+      .update({
+        name: new_name,
+        email: new_email,
+        password: new_password
+      })
+
+    return response.json()
   }
 }
 
--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -6,5 +6,6 @@ const usersRouter = new Router()
 const usersController = new UserController()
 
 usersRouter.post("/", usersController.create)
+usersRouter.put("/:id", usersController.update)
 
 module.exports = usersRouter
