b745b9da61026f9f47b393da66a1d6251de45d57
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-12 17:18:43 -0300

    add main tag on Home page

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,3 +1,4 @@
+import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 
 import { Container } from './styles'
@@ -6,6 +7,13 @@ export function Home() {
   return (
     <Container>
       <Header />
+
+      <main>
+        <div>
+          <h1>Meus Filmes</h1>
+          <Button title='Adicionar filme'/>
+        </div>
+      </main>
     </Container>
   )
 }
--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -2,4 +2,14 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   height: 100vh;
+
+  > main {
+    padding: 4.8rem 12.4rem 0;
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+  }
 `
