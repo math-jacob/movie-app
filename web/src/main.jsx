72dc2a2fd49df406acf315127c9758f24af67009
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-12 13:16:43 -0300

    add theme

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
 import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SignIn />
+    <ThemeProvider theme={theme}>
+      <SignIn />
+    </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ web/src/styles/theme.js
@@ -0,0 +1,18 @@
+export default {
+  COLORS: {
+    BACKGROUND: '#31C1B1E',
+    INPUT_BACKGROUND: '#262529',
+
+    PINK_100: '#FF859B',
+    PINK_800: 'rgba(255, 133, 155, 0.05)',
+    PINK_950: '#282124',
+
+    BLACK_100: '#1C1B1E',
+    BLACK_200: '#0D0C0F',
+
+    GRAY_100: '#CAC4CF',
+    GRAY_200: '#948F99',
+
+    WHITE: '#F4EDE8',
+  }
+}
