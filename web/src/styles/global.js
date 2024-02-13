d2f0ef3b43951b351ce3bffa943adddcb6664bee
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-13 16:59:12 -0300

    add custom scrollbar

--- web/src/styles/global.js
+++ web/src/styles/global.js
@@ -36,4 +36,28 @@ export default createGlobalStyle`
   button:hover, a:hover {
     filter: brightness(0.9);
   }
+
+  /* Scrollbar */
+  
+  /* Works on Firefox */
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: ${({theme}) => theme.COLORS.PINK_100} ${({theme}) => theme.COLORS.BLACK_100};
+  }
+
+  /* Works on Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 10px;
+    margin-left: 15px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: ${({theme}) => theme.COLORS.BLACK_100};
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: ${({theme}) => theme.COLORS.PINK_100};
+    border-radius: 20px;
+    border: 3px solid ${({theme}) => theme.COLORS.PINK_100};
+  }
 `
