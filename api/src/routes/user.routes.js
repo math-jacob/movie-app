720d2ae7c30e0dc8303f8fad6f5bb0f76fc8b77a
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-26 10:00:12 -0300

    handling user profile picture upload

--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,12 +1,19 @@
 const { Router } = require('express')
-const UserController = require('../controllers/UsersController')
+const multer = require('multer')
+const uploadConfig = require('../configs/upload')
 const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
 
+const UserController = require('../controllers/UsersController')
+const UserAvatarController = require('../controllers/UserAvatarController')
+
 const usersRouter = new Router()
+const upload = multer(uploadConfig.MULTER)
 
 const usersController = new UserController()
+const userAvatarController = new UserAvatarController()
 
-usersRouter.post("/", usersController.create)
-usersRouter.put("/", ensureAuthenticated, usersController.update)
+usersRouter.post('/', usersController.create)
+usersRouter.put('/', ensureAuthenticated, usersController.update)
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 module.exports = usersRouter
