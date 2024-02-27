6b4d0ea72b5fe996b66621534eaa134aebf965d0
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-27 08:08:23 -0300

    saving token and user data on local storage

--- web/src/contexts/auth.jsx
+++ web/src/contexts/auth.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useState, useEffect } from 'react'
 import { api } from '../services/api'
 
 const AuthContext = createContext({})
@@ -6,11 +6,23 @@ const AuthContext = createContext({})
 export function AuthProvider({ children }) {
   const [data, setData] = useState({})
 
+  useEffect(() => {
+    const user = localStorage.getItem('@rocketmovies:user')
+    const token = localStorage.getItem('@rocketmovies:token')
+  
+    if (user && token) {
+      api.defaults.headers.authorization = `Bearer ${token}`
+      setData({ user: JSON.parse(user), token })
+    }
+  }, [])
+  
   async function signIn({ email, password }) {
-
     try {
       const response = await api.post('/sessions', { email, password})
       const { user, token } = response.data
+
+      localStorage.setItem('@rocketmovies:user', JSON.stringify(user))
+      localStorage.setItem('@rocketmovies:token', token)
 
       api.defaults.headers.authorization = `Bearer ${token}`
       setData({ user, token })
