3c4359acceb98f03bfae999402ee24b26efbb001
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-27 15:34:21 -0300

    add movie states, handleAddTag and handleRemoveTag on NewMovie

--- web/src/pages/NewMovie/index.jsx
+++ web/src/pages/NewMovie/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
@@ -11,8 +12,23 @@ import { Button } from '../../components/Button'
 import { Container } from './styles'
 
 export function NewMovie() {
+  const [title, setTitle] = useState('')
+  const [rating, setRating] = useState(0)
+  const [description, setDescription] = useState('')
+
+  const [tags, setTags] = useState([])
+  const [newTag, setNewTag] = useState('')
 
   const navigate = useNavigate()
+
+  function handleAddTag() {
+    setTags(prevState => [...prevState, newTag])
+    setNewTag('')
+  }
+
+  function handleRemoveTag(deleted) {
+    setTags(prevState => prevState.filter(tag => tag !== deleted))
+  }
 
   return (
     <Container>
@@ -25,16 +41,39 @@ export function NewMovie() {
         <h1>Novo filme</h1>
 
         <div className='col-2'>
-          <Input placeholder='Título'/>
-          <Input placeholder='Sua nota (de 0 a 5)'/>
+          <Input 
+            placeholder='Título'
+            type='text'
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <Input 
+            placeholder='Sua nota (de 0 a 5)'
+            type='number'
+            onChange={(e) => setRating(e.target.value)}
+          />
         </div>
 
-        <Textarea placeholder='Observações'/>
+        <Textarea 
+          placeholder='Observações'
+          onChange={(e) => setDescription(e.target.value)}
+        />
 
         <h2>Marcadores</h2>
         <div className="tags-wrapper">
-          <InputTag value='React'/>
-          <InputTag isNew placeholder='Novo marcador'/>
+          {tags.map((tag, index) => (
+            <InputTag 
+              key={String(index)}
+              value={tag}
+              onClick={() => handleRemoveTag(tag)}
+            />
+          ))}
+          <InputTag 
+            isNew
+            value={newTag}
+            placeholder='Novo marcador'
+            onClick={handleAddTag}
+            onChange={e => setNewTag(e.target.value)}
+          />
         </div>
 
         <div className='col-2'>
