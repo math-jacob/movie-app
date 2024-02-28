2943a0b05d592d57b5e3105716849e0bf28e55d7
matheus-jacob <math.henrique.jacob@gmail.com>
2024-02-28 12:41:13 -0300

    changed button to on NewMovie

--- web/src/pages/NewMovie/index.jsx
+++ web/src/pages/NewMovie/index.jsx
@@ -105,7 +105,7 @@ export function NewMovie() {
         </div>
 
         <div className='col-2'>
-          <Button title='Excluir filme' type='button'/>
+          <Button title='Cancelar' type='button' onClick={() => navigate(-1)}/>
           <Button title='Salvar alterações' type='submit'/>
         </div>
       </Form>
